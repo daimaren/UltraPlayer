a7f5150a5a65bb725c8fbf6040595e97af280c82
daimaren <jianyongwang04@gmail.com>
2016-01-29 20:17:01 +0800

    audio

---
+++ app/src/main/java/cn/ixuehu/ultraplayer/ui/view/LyricView.java
@@ -0,0 +1,16 @@
+package cn.ixuehu.ultraplayer.ui.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * 项目名：UltraPlayer
+ * 包名：cn.ixuehu.ultraplayer.ui.view
+ * Created by daimaren on 2016/1/29.
+ */
+public class LyricView extends TextView{
+    public LyricView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
